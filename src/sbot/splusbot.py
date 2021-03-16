87191b8c690260d69aa3648b4637ff5b06513abd
misterbobot <misterbobot@yandex.ru>
2021-03-16 10:22:26 +0400

    small fixes

--- src/sbot/splusbot.py
+++ src/sbot/splusbot.py
@@ -16,6 +16,36 @@ class SPlusBot:
 
     #обновление координат робота
     def positionCallback(self,data : Odometry) -> None:
+
+        if (self.predPosition == Position(QuantPos(), -1 , -1)):
+
+            self.predPosition = self.position
+
+            self.position.x = data.pose.pose.position.x
+            self.position.y = data.pose.pose.position.y
+            self.position.theta.z = data.pose.pose.orientation.z
+            self.position.theta.x = data.pose.pose.orientation.x
+            self.position.theta.y = data.pose.pose.orientation.y
+            self.position.theta.w = data.pose.pose.orientation.w
+
+            return 
+        
+        dx = data.pose.pose.position.x - self.predPosition.x
+        dy = data.pose.pose.position.y - self.predPosition.y
+        
+        #TODO implement getting alpha value
+
+        alpha = 1
+
+        dx *= alpha
+        dy *= alpha
+
+        self.position.x += dx
+        slef.position.y += dy
+
+                        
+        self.predPosition = self.position 
+
         self.position.x = data.pose.pose.position.x
         self.position.y = data.pose.pose.position.y
         self.position.theta.z = data.pose.pose.orientation.z
@@ -52,7 +82,7 @@ class SPlusBot:
         
         self.ANGULAR_SPEED = config.DEFAULT_ANGULAR_SPEED
 
-        self.predPositions = Position(QuantPos(), -1 , -1)
+        self.predPosition = Position(QuantPos(), -1 , -1)
         
 
         self._getCurrentLocation()
