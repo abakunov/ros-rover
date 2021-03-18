71981600091b403ec65af08fbcbeaa46b2e10a04
misterbobot <misterbobot@yandex.ru>
2021-03-18 14:47:58 +0400

    fixes

--- src/sbot-server/main.py
+++ src/sbot-server/main.py
@@ -3,12 +3,14 @@ from flask import request
 import serial
 import struct
 import serial.tools.list_ports
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
-
+cors = CORS(app)
 serial_port = ''
 
 @app.route('/get_serial_ports/', methods=['GET'])
+@cross_origin()
 def get_serial_ports():
     comlist = serial.tools.list_ports.comports()
     connected = []
@@ -18,21 +20,25 @@ def get_serial_ports():
     return response
 
 @app.route('/set_serial_port/', methods=['POST'])
+@cross_origin()
 def set_serial_port():
     if request.method == "POST":
         global serial_port
-        serial_port = request.form['port']
+        data = eval(request.get_data().decode('utf-8'))
+        serial_port = data['port']
         return {'status': 'OK'}
     return {'status': 'Bad Request'}
 
 @app.route('/execute_command/', methods=['POST'])
+@cross_origin()
 def execute_command ():
     if request.method == "POST":
         global serial_port
-        command = request.form['command']
-        value = request.form['value']
+        data = eval(request.get_data().decode('utf-8'))
+        command = data['command']
+        value = data['value']
         command = eval("b'\\x"+command+"'")
-        
+        print(command)
         value = hex(int(value))[2::]
 
         f,s = "",""
@@ -48,11 +54,12 @@ def execute_command ():
         elif len(value) == 2:
             s = "\\x"+value[2:]
             f = "\\x"+value[:3:]
-        global serial_port
+
         
         
         strcuted_data = struct.pack('!3c', command, eval("b'"+f+"'") , eval("b'"+s+"'"))
         port = serial.Serial(serial_port, 19200)
+
         port.write(strcuted_data)
         return {'status': 'OK'}
 
