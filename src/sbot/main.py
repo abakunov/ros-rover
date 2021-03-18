e11776ad2a161b5d30f400d4362090022c0ac2c6
Ubercola <cola@cactus.ru>
2021-03-18 17:19:30 +0300

    fun

--- src/sbot/main.py
+++ src/sbot/main.py
@@ -11,14 +11,13 @@ print("bot have been inited")
 #print(bot.position.theta.toTheta())
 
 #bot.move_forward(0.4,0.2)
-bot.move2point(Point(0.5,0.5))
-sleep(5)
-bot.move2point(Point(-1,0.5))
+bot.move2goal(Point(0.5,0.5))
+
 #print(bot.position.theta.toTheta())
 #bot.stop() 
 #bot.rotate(349,0.3)
 #bot.rotate2angle(0)
 #bot.servoRotateHorizontal(20)
-#sleep(2)
+sleep(2)
 #print(bot.position.theta.toTheta())
 
--- src/sbot/splusbot.py
+++ src/sbot/splusbot.py
@@ -2,7 +2,7 @@ import rospy
 import config
 import time
 import numpy as np
-
+from math import sqrt,atan2
 from nav_msgs.msg import Odometry
 from geometry_msgs.msg import Twist
 
@@ -185,4 +185,58 @@ class SPlusBot:
     def turnOffLed(self):
         self.led.turn_off()
         
-    
+
+    #################################
+
+
+    def euclidean_distance(self, goal_pose):
+        """Euclidean distance between current pose and the goal."""
+        return sqrt(pow((goal_pose.x - self.position.x), 2) +
+                    pow((goal_pose.y - self.position.y), 2))
+    def linear_vel(self, goal_pose, constant=0.1):
+        """See video: https://www.youtube.com/watch?v=Qh15Nol5htM."""
+
+        return constant * self.euclidean_distance(goal_pose)
+
+    def steering_angle(self, goal_pose):
+        """See video: https://www.youtube.com/watch?v=Qh15Nol5htM."""
+        return atan2(goal_pose.y - self.position.y, goal_pose.x - self.position.x)
+
+    def angular_vel(self, goal_pose, constant=0.005):
+        """See video: https://www.youtube.com/watch?v=Qh15Nol5htM."""
+
+        return constant * (self.steering_angle(goal_pose) - self.position.theta.toTheta())
+
+    def move2goal(self,goal_pose):
+        """Moves the turtle to the goal."""
+
+        # Please, insert a number slightly greater than 0 (e.g. 0.01).
+        distance_tolerance = 0.05
+
+        vel_msg = Twist()
+        rate = rospy.Rate(config.DEFAULT_LOOP_RATE) 
+        while self.euclidean_distance(goal_pose) >= distance_tolerance:
+
+            # Porportional controller.
+            # https://en.wikipedia.org/wiki/Proportional_control
+
+            # Linear velocity in the x-axis.
+            vel_msg.linear.x = self.linear_vel(goal_pose)
+            vel_msg.linear.y = 0
+            vel_msg.linear.z = 0
+
+            # Angular velocity in the z-axis.
+            vel_msg.angular.x = 0
+            vel_msg.angular.y = 0
+            vel_msg.angular.z = self.angular_vel(goal_pose)
+
+            # Publishing our vel_msg
+            self.velPub.publish(vel_msg)
+
+            # Publish at the desired rate.
+            rate.sleep()
+
+        # Stopping our robot after the movement is over.
+        vel_msg.linear.x = 0
+        vel_msg.angular.z = 0
+        self.velPub.publish(vel_msg)
