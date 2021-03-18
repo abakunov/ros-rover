4ef57afce02bed4a831860f84d4bdc8200846309
misterbobot <misterbobot@yandex.ru>
2021-03-18 15:31:39 +0400

    small fixes in the test file

--- src/sbot/main.py
+++ src/sbot/main.py
@@ -4,14 +4,16 @@ from classes import Position,Point
 import serial
 import numpy as np
  
-#bot = SPlusBot()
+bot = SPlusBot()
 
-#print("oinit")
+print("bot have been inited")
 
 #print(bot.position.theta.toTheta())
 
 #bot.move_forward(0.4,0.2)
-#bot.move2point(Point(1,1))
+bot.move2point(Point(0.5,0.5))
+sleep(5)
+bot.move2point(Point(-1,0.5))
 #print(bot.position.theta.toTheta())
 #bot.stop() 
 #bot.rotate(349,0.3)
@@ -20,29 +22,3 @@ import numpy as np
 #sleep(2)
 #print(bot.position.theta.toTheta())
 
-def move2point(p1:Point, point : Point):
-        
-    bot_pos = p1
-        
-    hyp = p1 - point
-        
-    x_diff = (bot_pos.x - point.x)
-    y_diff = (bot_pos.y - point.y)
-    
-
-    deg = abs(np.degrees(np.arctan(y_diff / x_diff)))
-
-    if y_diff < 0:
-            if x_diff < 0:
-                print(deg,"+")
-            else:
-                print(360 - deg,"-")
-    else:
-            if x_diff < 0:
-                print((90 + deg)%360,"+")
-
-            else:
-                print((180 + deg)%360,"-")
-
-
-move2point(Point(0,0), Point(1,-1))
